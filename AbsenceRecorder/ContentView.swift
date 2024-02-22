23e355d0dcfb9517432d0aceec47e4d64b44bd93
OGSmithy <smith.o@etoncollege.org.uk>
2024-02-22 21:03:23 +0000

    Initial Commit

---
+++ AbsenceRecorder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AbsenceRecorder
+//
+//  Created by Smith, Oli (AMM) on 22/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
